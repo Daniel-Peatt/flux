a20de5127f1a4ce9ba11b0a15df118b18a6f2338
Daniel Peatt <danielpeatt@gmail.com>
2025-02-26 18:28:47 -0500

    Add restrictions to CreateAccount

---
+++ backend/database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE user_data (
+	challenge_id SERIAL PRIMARY KEY,
+	user_id INT NOT NULL,
+	title VARCHAR(100) NOT NULL, 
+	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
--- src/components/CreateAccount/CreateAccount.jsx
+++ src/components/CreateAccount/CreateAccount.jsx
@@ -12,18 +12,51 @@ function CreateAccountPage () {
 
     const navigate = useNavigate();
 
+    // Used to validate a email address
+    const validateEmail = (email) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    };
+
     // Sends new account information to the database
     const onSubmitForm = async(e) => {
         e.preventDefault(); // keeps it from refreshing 
         try {
-            const body = {email, password_hash};
+            // Information from form
+            const body = {email, password_hash, confirmPassword};
+
+            // Check if email is valid
+            if (!validateEmail(email))
+            {
+                setErrorMessage("Not a valid email.")
+                return;
+            }
+
+            // Check if passwords match
+            if (password_hash != confirmPassword)
+            {
+                setErrorMessage("Passwords do not match.")
+                return;
+            }
+
+            // Check if the password meets requirments
+            if (password_hash.length < 8)
+            {
+                setErrorMessage("Password must be at least 8 characters.")
+                return;
+            }
+
+            // Sending the POST request to the database
             const response = await fetch("http://localhost:5000/createUser", { // API ROUTE
                 method: "post", // Sets type of API request
                 headers: {"content-type": "application/json"}, // Sets the type to JSON
                 body: JSON.stringify(body) // Sets and converts the body into a JSON
             });
+            
+            // Sending response to the console
             const result = await response.json();
             console.log(result);
+
+            // If the response status is 409, the email already exist in the database. 
             if (response.status === 409) {
                 setErrorMessage("Email is already taken.");
                 return;
@@ -31,9 +64,10 @@ function CreateAccountPage () {
                 setErrorMessage(result.error || "Something went wrong. Please try again.");
                 return;
             }    
+
             // If successful, routes the user to the CreateTitle page
             if (response.ok) {
-                // Navigate to another page after successful registration
+                // Navigate to the CreateTitle page after successful registration
                 navigate("/CreateTitle"); 
             }
         } catch (err) {
